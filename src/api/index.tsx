630ddbde3194f5f7fdd3b2769b82c41f19e9a615
최현호 <x7788@naver.com>
2022-12-26 13:07:27 +0900

    feat : api 주소 변경 및 게시판 이미지 추가

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-    baseURL : 'http://106.247.124.178:3060/',
+    baseURL : 'http://124.55.62.184:3060/',
     timeout : 10000,
 });
 
